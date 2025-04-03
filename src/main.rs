c9d1d2b1ae1f0192f4d23c0eebb23778dbe9ef22
Max <msrylkin@gmail.com>
2025-04-03 23:51:18 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -11,26 +11,89 @@ use ark_poly::univariate::{DenseOrSparsePolynomial, DensePolynomial};
 use ark_ec::short_weierstrass::{Affine, SWCurveConfig};
 use ark_ff::Field;
 use ark_poly::Polynomial;
-use ark_test_curves::bls12_381::g1::Config;
+use ark_test_curves::bls12_381::g1::Config as G1Config;
+use ark_test_curves::bls12_381::g2::Config as G2Config;
 
-
+use ark_poly::DenseUVPolynomial;
+use ark_std::iterable::Iterable;
 
 fn main() {
-    let mut rnd = ark_std::test_rng();
-    let e1 = G1::rand(&mut rnd);
-    let e2 = G2::rand(&mut rnd);
-    // println!("{} {}", e1, e2);
-    // println!("{:?}", Fr::get_root_of_unity(8));
-    kzg();
-}
-
-fn setup() {
-    let rng = &mut ark_std::test_rng();
-    let tau = Fr::rand(rng);
-}
-
-fn commit_to_poly(poly: &DensePolynomial<Fq>) -> () {
+    let polynomial = DensePolynomial::from_coefficients_vec(
+        vec![Fr::from(11), Fr::from(12), Fr::from(345)]
+    );
     
+    let (srs, tau_g2) = setup();
+    let commitment = commit_to_poly(&polynomial, &srs);
+    
+    let random_a = Fr::from(777);
+    
+    let (f_a, evaluation_proof) = open_commitment(&srs, &random_a, &polynomial);
+    
+    let is_valid = verify_commitment(&commitment, &tau_g2, &random_a, &f_a, &evaluation_proof);
+    
+    println!("is_valid = {}", is_valid);
+}
+
+fn setup() -> (Vec<Affine<G1Config>>, Affine<G2Config>) {
+    let max_degree = 8;
+    let rng = &mut ark_std::test_rng();
+    
+    let tau = Fr::rand(rng);
+    
+    let tau_g2 = (G2::generator() * tau).into_affine();
+    let srs = (0..max_degree).map(|d| {
+        let tau_times_d  = tau.pow(&[d]);
+        G1::generator().mul(tau_times_d).into_affine()
+    }).collect::<Vec<_>>();
+
+    (srs, tau_g2)
+}
+
+fn commit_to_poly(
+    poly: &DensePolynomial<Fr>,
+    srs: &Vec<Affine<G1Config>>
+) -> Affine<G1Config> {
+    let comm_f = eval_poly_on_srs(poly, srs);
+    
+    comm_f
+}
+
+fn open_commitment(
+    srs: &Vec<Affine<G1Config>>,
+    // tau_g2: &Affine<G2Config>,
+    a: &Fr,
+    poly: &DensePolynomial<Fr>,
+) -> (Fr, Affine<G1Config>) {
+    let f_a = poly.evaluate(a);
+    
+    let f_a_poly = DensePolynomial::from_coefficients_vec(vec![f_a]);
+    let x_minus_a_poly = DensePolynomial::from_coefficients_vec(vec![-*a, Fr::one()]);
+    let q_x = poly.sub(&f_a_poly).div(&x_minus_a_poly);
+    
+    let evaluation_proof = eval_poly_on_srs(&q_x, srs);
+
+    (f_a, evaluation_proof)
+}
+
+fn verify_commitment(
+    commitment: &Affine<G1Config>,
+    // srs: &Vec<Affine<G1Config>>,
+    tau_g2: &Affine<G2Config>,
+    a: &Fr,
+    f_a: &Fr,
+    evaluation_proof: &Affine<G1Config>,
+    // poly: &DensePolynomial<Fr>,
+) -> bool {
+    let l_pairing = Bls12_381::pairing(
+        evaluation_proof,
+        *tau_g2 - G2::generator() * a,
+    );
+    let r_pairing = Bls12_381::pairing(
+        *commitment - G1::generator() * f_a, 
+        G2::generator(),
+    );
+    
+    l_pairing == r_pairing
 }
 
 fn kzg() {
@@ -121,8 +184,12 @@ fn kzg() {
 
 fn eval_poly_on_srs(
     poly: &DensePolynomial<Fr>,
-    srs: &[Affine<Config>]
-) -> Affine<Config> {
+    srs: &[Affine<G1Config>]
+) -> Affine<G1Config> {
+    if srs.len() < poly.degree() {
+        panic!("srs lower than poly degree");
+    }
+
     poly.coeffs
         .iter()
         .zip(srs)

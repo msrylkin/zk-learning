3802023f1297c961b63b806553f63f70d40afd18
Max <msrylkin@gmail.com>
2025-06-12 16:45:35 +0300

    sumcheck

---
+++ src/gkr.rs
@@ -0,0 +1,18 @@
+
+
+struct Input<F> {
+    value: F
+}
+
+// struct Gate {
+//     inputs: [Gate; 2],
+// }
+
+// struct Level {
+//     index: usize,
+//     gates: Vec<Gate>,
+// }
+
+// struct Circuit {
+//     levels: Vec<Level>,
+// }

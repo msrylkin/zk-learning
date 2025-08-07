af06299b7d470a5f0d330c299229d81443593482
mrylkin <m.rylkin@itez.com>
2025-08-07 21:08:50 +0300

    multilinear poly sumcheck tests

--- src/sumcheck/multilin_sumcheck_poly.rs
+++ src/sumcheck/multilin_sumcheck_poly.rs
@@ -2,34 +2,11 @@ use std::ops::Deref;
 use ark_ff::Field;
 use ark_poly::{DenseMultilinearExtension, MultilinearExtension, Polynomial};
 use ark_poly::univariate::DensePolynomial;
-use crate::poly_utils::interpolate_univariate_on_evals;
+use crate::poly_utils::{interpolate_univariate_on_evals};
 use crate::sumcheck::OracleEvaluation;
 use crate::sumcheck::sumcheck_poly::SumCheckPoly;
 
-// 000 |
-// 001 --> 00[1 + 0]
-// 010 |
-// 011 --> 01[1 + 0]
-fn sum_over_last_variable<F: Field>(poly: &DenseMultilinSumcheckPoly<F>) -> DenseMultilinSumcheckPoly<F> {
-    let sums = poly
-        .evaluations
-        .iter()
-        .enumerate()
-        .fold(vec![F::zero(); poly.evaluations.len() / 2], |mut sums, (i, e)| {
-            if i % 2 == 0 {
-                sums[(i / 4) * 2] += e;
-            } else {
-                sums[(i / 4) * 2 + 1] += e;
-            }
-            sums
-        });
-
-    let res = DenseMultilinSumcheckPoly::from_evaluations_vec(poly.num_vars - 1, sums);
-
-    res
-}
-
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct DenseMultilinSumcheckPoly<F: Field>(DenseMultilinearExtension<F>);
 
 impl<F: Field> DenseMultilinSumcheckPoly<F> {
@@ -38,6 +15,19 @@ impl<F: Field> DenseMultilinSumcheckPoly<F> {
     }
     pub fn from_evaluations_vec(num_vars: usize, evals: Vec<F>) -> Self {
         Self(DenseMultilinearExtension::from_evaluations_vec(num_vars, evals))
+    }
+    
+    pub fn sum_over_last_variable(&self) -> DenseMultilinSumcheckPoly<F> {
+        let new_vars_num = self.num_vars() - 1;
+
+        let res = (0..1 << new_vars_num).map(|i| {
+            let index = i;
+            let partner_index = i | (1 << new_vars_num);
+
+            self.evaluations[index] + self.evaluations[partner_index]
+        }).collect::<Vec<_>>();
+        
+        DenseMultilinSumcheckPoly::from_evaluations_vec(new_vars_num, res)
     }
 }
 
@@ -58,15 +48,19 @@ impl<F: Field> SumCheckPoly<F> for DenseMultilinSumcheckPoly<F> {
     }
 
     fn get_partial_sum_poly(&self) -> DensePolynomial<F> {
-        let sum_vars = self.num_vars() - 1;
-        let mut current_poly = self.clone();
+        let (zeroes, ones) = self
+            .evaluations
+            .iter()
+            .enumerate()
+            .fold((F::zero(), F::zero()), |(zeroes, ones), (i, e)| {
+                if i & 1 == 0 {
+                    (zeroes + e, ones)
+                } else {
+                    (zeroes, ones + e)
+                }
+            });
 
-        for _ in 0..sum_vars {
-            let summed_poly = sum_over_last_variable(&current_poly);
-            current_poly = summed_poly;
-        }
-
-        interpolate_univariate_on_evals(&current_poly.get_evaluations().try_into().unwrap())
+        interpolate_univariate_on_evals(&[zeroes, ones])
     }
 
     fn fix_variable(&self, e: F) -> Self {
@@ -89,5 +83,58 @@ impl<F: Field> DenseMultilinFinalEvaluationOracle<F> {
 impl<F: Field> OracleEvaluation<F> for DenseMultilinFinalEvaluationOracle<F> {
     fn final_eval(&self, r: &[F]) -> F {
         self.0.evaluate(&r.to_vec())
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use ark_ff::Field;
+    use ark_poly::{MultilinearExtension};
+    use ark_test_curves::bls12_381::Fr;
+    use crate::poly_utils::{to_f};
+    use crate::sumcheck::multilin_sumcheck_poly::{DenseMultilinSumcheckPoly};
+    use crate::sumcheck::SumCheckPoly;
+    
+    fn test_poly<const VARS: usize, F: Field>() -> DenseMultilinSumcheckPoly<F> {
+        let values = match VARS {
+            1 => vec![10, 20],
+            2 => vec![10, 300, 20, 400],
+            3 => vec![10, 55, 300, 77, 20, 66, 400, 88],
+            _ => panic!(),
+        };
+        
+        DenseMultilinSumcheckPoly::from_evaluations_vec(VARS, to_f(values))
+    }
+    
+    #[test]
+    fn sum_over_last_variable_test_3_vars() {
+        let poly = test_poly::<3, Fr>();
+        
+        let poly = poly.sum_over_last_variable();
+        
+        assert_eq!(poly.num_vars(), 2);
+        assert_eq!(poly.to_evaluations(), to_f::<Fr>(vec![30, 121, 700, 165]));
+
+        let poly = poly.sum_over_last_variable();
+
+        assert_eq!(poly.num_vars(), 1);
+        assert_eq!(poly.to_evaluations(), to_f::<Fr>(vec![730, 286]));
+    }
+
+    #[test]
+    fn test_partial_sum_poly_3var() {
+        let poly = test_poly::<3, Fr>();
+
+        let partial_sum_poly = poly.get_partial_sum_poly();
+        assert_eq!(partial_sum_poly.coeffs, to_f::<Fr>(vec![730, -444]));
+    }
+
+    #[test]
+    fn test_partial_sum_poly_2var() {
+        let poly = test_poly::<2, Fr>();
+
+        let partial_sum_poly = poly.get_partial_sum_poly();
+        assert_eq!(partial_sum_poly.coeffs, to_f::<Fr>(vec![30, 670]));
     }
 }
